60260cc7c934956158699aa17681da02a15d2a68
ayhanunal <unalayhan@yahoo.com>
2021-07-10 21:01:56 +0300

    main branch toast added.

--- app/src/main/java/com/ayhanunal/gitbranchdemo/MainActivity.kt
+++ app/src/main/java/com/ayhanunal/gitbranchdemo/MainActivity.kt
@@ -3,12 +3,15 @@ package com.ayhanunal.gitbranchdemo
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(applicationContext, "Welcome", Toast.LENGTH_SHORT).show()
 
 
         deviceModelButton.setOnClickListener {
