31ed7a3b7af70881ff714e8f91acf207604e0d40
ayhanunal <unalayhan@yahoo.com>
2021-07-10 20:17:02 +0300

    basic device manufacturer and model info

--- app/src/main/java/com/ayhanunal/gitbranchdemo/MainActivity.kt
+++ app/src/main/java/com/ayhanunal/gitbranchdemo/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.ayhanunal.gitbranchdemo
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        deviceModelButton.setOnClickListener {
+            modelText.text = "Model : ${Build.MANUFACTURER} / ${Build.MODEL} "
+        }
+
+
     }
+
 }
